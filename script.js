7ac42db7581521fec4e7829a211edb6632493fa3
Henrique Aquino <rickybrasileiro1@gmail.com>
2023-02-06 09:42:07 -0300

    Funcoes de clicar

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function clicou (){
+
+document.querySelector("div").innerText = "clicou"
+
+  
+}
+
+function passou (){
+
+    document.querySelector("div").innerText = "Passou o Mouse"
+
+}
+
+
+function  Tirou (){
+document.querySelector("div").innerText = "Tirou"
+
+
+}
