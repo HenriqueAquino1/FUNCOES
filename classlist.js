fbaf246eacc23a25a653f4e7313f6f823d075697
Henrique Aquino <rickybrasileiro1@gmail.com>
2023-02-07 11:29:49 -0300

    Atividade Classlist

---
+++ classlist.js
@@ -0,0 +1,12 @@
+
+function Clique(){
+
+    document.querySelector("#minhaDiv").classList.add("classe1","classe2","classe3")
+
+}
+
+function Remover(){ 
+
+ document.querySelector("#minhaDiv").classList.remove("classe1","classe2","classe3")
+
+}
---
+++ terça/elementos.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+function Clicou() {
+
+
+
+const paragrafo = document.createElement('p')
+paragrafo.innerText="Isso é um paragrafo"  
+
+document.body.appendChild(paragrafo)
+
+}
